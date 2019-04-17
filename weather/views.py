8daf8107c66aec52ee78be7037b5b76d3422f411
Akzhol Imangaliyev <akzhol.im@gmail.com>
2019-04-17 10:46:18 +0600

    finished design

--- weather/views.py
+++ weather/views.py
@@ -10,7 +10,7 @@ from .forms import CityForm
 def index(request):
     # OpenWeatherMap API endpoint
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}'
-    api_key = 'your API key from openweathermap.org'
+    api_key = 'c4af811c05a1cdeea2526001714ae63c'
 
     if request.method == 'POST':
         form = CityForm(request.POST)
@@ -34,6 +34,7 @@ def index(request):
             'temperature': r['main']['temp'],
             'description': r['weather'][0]['description'],
             'icon': r['weather'][0]['icon'],
+            'country': r['sys']['country']
         }
 
         weather_data.append(city_weather)

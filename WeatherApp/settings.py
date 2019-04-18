f7df12aebac02bf17437a27809fb4de0c67aab47
Akzhol Imangaliyev <akzhol.im@gmail.com>
2019-04-18 15:55:34 +0600

    added summary for each city when the card is clicked

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -130,4 +130,4 @@ DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
